5ca5910d4293e404b2536c007afa56f37f41ec47
John Lee <ljonghan@gmail.com>
2022-07-19 14:41:47 -0400

    added lambda expression

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,8 +6,17 @@ public class Main {
     public static void main(String[] args) {
         int num = 5;
         int num2 = 10;
-
+        OddNumber nextOddNum = (number) -> {
+            if(number % 2 == 0) return number + 1;
+            return number + 2;
+        };
         System.out.println(nextOddNum.applyAsInt(num)); // 7
         System.out.println(nextOddNum.applyAsInt(num2)); // 11
     }
 }
+
+@FunctionalInterface
+interface OddNumber{
+    int applyAsInt(int num);
+}
+
